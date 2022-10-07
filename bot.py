70ca284a56e02058f584cb4f297c826bf8a37813
MaybeCaat <iemm@rambler.ru>
2022-10-07 21:16:10 +0300

    add new admin button and last menu

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 # ЗАМЕНИТЬ ФРАЗЫ НА ФИНАЛЬНЫЕ
 # ЗАМЕНИТЬ XLS ФАЙЛ С ДАННЫМИ
 # РЕФАКТОР КОДА
+# НАСТРОИТЬ ПРАВИЛЬНО ГДЕ-ТО REPLY, А ГДЕ-ТО ANSWER
 
 # Объект бота
 bot = Bot(token="5763817832:AAHryUF70705YJcIOraaYVh1njprvtS752Y")
@@ -23,12 +24,23 @@ logging.basicConfig(level=logging.INFO)
 adminPasswords = {0: 'Вероника', 1: 'VxWorks'}
 adminID = None
 isLogin = False
+acceptedCargos = 0
+cargoNumbers = [72121022, 12102006, 78151290, 55005321]
 
 
 class Form(StatesGroup):
     adminPassword = State()
     answerFlyControl = State()
     answerUpdateSoftware = State()
+    answerCargoDumping = State()
+    answerAutoPilot = State()
+
+
+# Колбэк на кнопку "Подсказка"
+@dp.callback_query_handler(text="Подсказка")
+async def _(call: types.CallbackQuery):
+    print("Подсказка callback")
+    await call.message.answer("49.272598, -123.132872 város?")
 
 
 # Хэндлер на команду /start
@@ -59,7 +71,13 @@ async def _(call: types.CallbackQuery):
 # Колбэк на кнопку "Сброс груза"
 @dp.callback_query_handler(text="Сброс груза")
 async def _(call: types.CallbackQuery):
-    await call.message.answer("Отправка груза заблокирована. Для выполнения этого действия необходимы права администратора.")
+    if adminID is None:
+        await call.message.answer(
+            "Отправка груза заблокирована. Для выполнения этого действия необходимы права администратора.")
+        return
+    await Form.answerCargoDumping.set()
+    await call.message.answer(
+        "Введите номер груза, который вы хотите вернуть отправителю. За раз можно отправить РОВНО 4 контейнера")
 
 
 # Хэндлер на команду /admin
@@ -134,13 +152,6 @@ async def _(call: types.CallbackQuery):
                               reply_markup=keyboard)
 
 
-# Колбэк на кнопку "Подсказка"
-@dp.callback_query_handler(text="Подсказка")
-async def _(call: types.CallbackQuery):
-    print("Подсказка callback")
-    await call.message.answer("49.272598, -123.132872 város?")
-
-
 # Проверка ответа на "Управление полётом"
 @dp.message_handler(state=Form.answerFlyControl)
 async def processFlyControl(message: types.Message, state: FSMContext):
@@ -200,11 +211,63 @@ async def processUpdateSoftware(message: types.Message, state: FSMContext):
     await message.answer('Обновление программного обеспечение установлено')
 
 
+# Проверка ответа на "Сброс груза"
+@dp.message_handler(state=Form.answerCargoDumping)
+async def processCargoDumping(message: types.Message, state: FSMContext):
+    await state.finish()
+    global cargoNumbers
+    if message.text.isdigit() is False or int(message.text) not in cargoNumbers:
+        await message.reply('Некорректный номер груза. Проверьте правильность ввода и повторите отправку сообщения')
+        await Form.answerCargoDumping.set()
+        return
+    cargoNumbers.remove(int(message.text))
+    if len(cargoNumbers) != 0:
+        await message.reply('Груз готовится к отправке! Введите следующий номер')
+        await Form.answerCargoDumping.set()
+        return
+    await message.answer('Набор для доставки сформирован. Следующая доставка будет доступна через 42 часа')
+    return
+
+
 # Колбэк на кнопку "Данные"
 @dp.callback_query_handler(text="Данные")
 async def _(call: types.CallbackQuery):
     xls_path = "data.xlsx"
     await call.message.answer_document(InputFile(xls_path))
+
+
+# Колбэк на кнопку "Режим автопилота"
+@dp.callback_query_handler(text="Режим автопилота")
+async def _(call: types.CallbackQuery):
+    await Form.answerAutoPilot.set()
+    await call.message.answer("Введите ключ верификации, состоящий из 9 строчных латинских символов:")
+
+
+# Проверка ответа на "Режим автопилота"
+@dp.message_handler(state=Form.answerAutoPilot)
+async def processAutopilot(message: types.Message, state: FSMContext):
+    await state.finish()
+    if message.text != 'Vancouver':
+        await message.reply('Ключ верификации неверен. Попробуйте снова.')
+        await Form.answerAutoPilot.set()
+        return
+    await message.reply('Станция переведена в режим автопилота. Ручное управление отключено. '
+                        'Вы будете переведены в статус контролера')
+    # Завершающая стадия квеста
+    await asyncio.sleep(180)
+    await message.answer('Ошибка корректировки траектории! '
+                         'Для исправления ошибки будет сброшено несколько резервных грузовых отсеков')
+    await asyncio.sleep(60)
+    await message.answer("""
+    Координаты сброшенных грузов:
+    43.400330, 39.974873
+    43.595834, 39.929211
+    44.606875, 39.992500
+    45.254240, 39.674883
+    """)
+    await message.answer('Один из грузов приземлился рядом с вашим местоположением.'
+                         'Вам необходимо забрать его, во избежание загрязнения окружающей среды.'
+                         'Содержимое груза вы можете оставить себе')
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
